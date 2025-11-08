59eaecbf5b13723d40e1ee3f6f62c40196e82a28
Anthony <thesuper32203@gmail.com>
2025-11-08 13:29:05 -0500

    startning plaidService

--- src/main/java/com/example/plaidapp/plaid_app/service/PlaidService.java
+++ src/main/java/com/example/plaidapp/plaid_app/service/PlaidService.java
@@ -1,5 +1,6 @@
 package com.example.plaidapp.plaid_app.service;
 
+import com.example.plaidapp.plaid_app.config.PlaidConfig;
 import com.plaid.client.ApiClient;
 import com.plaid.client.request.PlaidApi;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +11,5 @@ import java.util.HashMap;
 @Service
 public class PlaidService {
 
-    private PlaidApi client;
 
-    @Bean
-    public void getClient() {
-        if (client == null) {
-            HashMap<String,String> apiKeys = new HashMap<String,String>();
-            apiKeys.put("clientId", )
-            ApiClient apiClient = new ApiClient()
-        }
-    }
 }
