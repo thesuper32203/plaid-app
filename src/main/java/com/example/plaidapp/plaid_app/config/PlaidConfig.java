118172fb89e05bef6e966fc4efd8edf57e0b4c24
Anthony <thesuper32203@gmail.com>
2025-11-08 13:24:16 -0500

    finished plaidConfig

--- src/main/java/com/example/plaidapp/plaid_app/config/PlaidConfig.java
+++ src/main/java/com/example/plaidapp/plaid_app/config/PlaidConfig.java
@@ -1,4 +1,22 @@
 package com.example.plaidapp.plaid_app.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
 public class PlaidConfig {
+
+    @Value("${plaid.client-id}")
+    private String clientId;
+
+    @Value("${plaid.secret}")
+    private String secret;
+
+    @Value("${plaid.env}")
+    private String environment;
+
 }
--- src/main/java/com/example/plaidapp/plaid_app/service/PlaidService.java
+++ src/main/java/com/example/plaidapp/plaid_app/service/PlaidService.java
@@ -1,4 +1,23 @@
 package com.example.plaidapp.plaid_app.service;
 
+import com.plaid.client.ApiClient;
+import com.plaid.client.request.PlaidApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
 public class PlaidService {
+
+    private PlaidApi client;
+
+    @Bean
+    public void getClient() {
+        if (client == null) {
+            HashMap<String,String> apiKeys = new HashMap<String,String>();
+            apiKeys.put("clientId", )
+            ApiClient apiClient = new ApiClient()
+        }
+    }
 }
