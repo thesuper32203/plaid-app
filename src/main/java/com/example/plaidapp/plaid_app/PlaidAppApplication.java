cc5337909f67534ba60b5309bf06068ade2290d5
Anthony <thesuper32203@gmail.com>
2025-11-08 11:20:38 -0500

    init project

---
+++ src/main/java/com/example/plaidapp/plaid_app/PlaidAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.plaidapp.plaid_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaidAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaidAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plaidapp/plaid_app/PlaidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plaidapp.plaid_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
