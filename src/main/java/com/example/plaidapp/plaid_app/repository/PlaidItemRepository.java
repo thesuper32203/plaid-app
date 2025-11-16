91f2633d7c45096f41ae674da84b6831d73fbd1f
Anthony <thesuper32203@gmail.com>
2025-11-16 12:42:30 -0500

    Fixed mismatch error in plaidItemRepository

--- src/main/java/com/example/plaidapp/plaid_app/repository/PlaidItemRepository.java
+++ src/main/java/com/example/plaidapp/plaid_app/repository/PlaidItemRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PlaidItemRepository extends JpaRepository<PlaidItem, Long> {
+public interface PlaidItemRepository extends JpaRepository<PlaidItem, String> {
     boolean existsByItemIdAndUserId(String itemId, String userId);
 
     PlaidItem findByItemId(String itemId);
